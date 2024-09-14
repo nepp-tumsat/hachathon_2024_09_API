71a75045e9d84b213e09558ced13e24462d3620e
Doragon-RH <r216af@icloud.com>
2024-09-14 23:00:55 +0900

    CORSのための追記

--- api/main.py
+++ api/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from api.routers import task, done
 
 app = FastAPI()
 app.include_router(task.router)
 app.include_router(done.router)
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # @app.get("/hello")
 # async def hello():
 #     return {"Hello": "World"}
