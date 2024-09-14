ca7ed84fc11f465b1330e4b4506ea6abb7ec8202
takutq <takuofromaomori2525@au.com>
2024-09-14 16:46:39 +0900

    star_limited

--- api/models/task.py
+++ api/models/task.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Time, CheckConstraint
 from sqlalchemy.orm import relationship
 
 from api.db import Base
@@ -10,8 +10,8 @@ class Task(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(1024))
 
-    created_at = Column(nullable=False, default=datetime.datetime.now)   # 追加(作成日時)
-    stars = Column(Integer, nullable=True)  # 追加(星の数)
+    created_at = Column(Time,nullable=False, default=datetime.datetime.now)   # 追加(作成日時)
+    stars = Column(Integer, CheckConstraint('stars >=0 AND stars <=5'), nullable=False)  # 追加(星の数)
     comment = Column(String(200))   # 追加(本へのコメント)
 
     done = relationship("Done", back_populates="task", cascade="delete")  
